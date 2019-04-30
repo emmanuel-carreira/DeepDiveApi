10cb9537054fc4099b9fc6b94bde0154e0db8b26
emmanuel-carreira <eca2@cin.ufpe.br>
2019-04-30 09:36:37 -0300

    added spring boot dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,17 +12,24 @@ plugins {
 
     // Apply the application plugin to add support for building an application
     application
+
+    id("org.springframework.boot") version "2.1.4.RELEASE"
 }
 
 repositories {
     // Use jcenter for resolving your dependencies.
     // You can declare any Maven/Ivy/file repository here.
     jcenter()
+    mavenCentral()
 }
 
 dependencies {
+    
     compileOnly("org.projectlombok:lombok:1.18.6")
     annotationProcessor("org.projectlombok:lombok:1.18.6")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
     // This dependency is found on compile classpath of this component and consumers.
     implementation("com.google.guava:guava:27.0.1-jre")
 
